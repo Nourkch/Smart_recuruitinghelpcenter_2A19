0733596863420e82c809e899b123046228af6094
Nourkch <80201640+Nourkch@users.noreply.github.com>
2021-10-18 20:53:10 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "dialog.h"
+
+
+// test tuto git
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    setCentralWidget(ui->groupBox);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +21,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionCommentaires_triggered()
+{
+
+   Dialog dialog;
+
+   dialog.setModal(true);
+   dialog.exec();
+
+}
